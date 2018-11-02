2f7f69f0de67611f66a060a55bfb2003303ec4f1
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 00:50:27 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,14 +15,8 @@ def long_planeteer_calls(calls)
   end
 end 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(queso)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
-
-# def summon_captain_planet(array)
-#   array.collect do |word| 
-#     word.capitalize + "!"
-#   end 
-# end 
