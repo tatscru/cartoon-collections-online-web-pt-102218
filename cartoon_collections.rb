048dc8f86096a638329600d08671528ee2384b36
tatiana.mojica <tatiana.mojica@bustle.com>
2018-11-02 00:44:58 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,14 +1,17 @@
 def roll_call_dwarves(names)
-  names.each_with_index {|name, index|} 
+  names.each_with_index {|name, index| puts "#{index + 1}. *#{name}"}
+  # we want names to return the name and corresponding index number. puts goes after called argument 
+end 
+
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect {|i| i + "!"} 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def long_planeteer_calls(calls) 
+  calls.any? do |word|
+    word.length > 4
 end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
+end 
 
 def find_the_cheese# code an argument here
   # the array below is here to help
